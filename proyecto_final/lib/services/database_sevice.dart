8ede45946c900981f2c93a2c541125b67585e1cd
Brian <brianleonoff@gmail.com>
2024-04-21 20:17:36 -0300

    database coneccion crud

--- proyecto_final/lib/services/database_sevice.dart
+++ proyecto_final/lib/services/database_sevice.dart
@@ -1,3 +1,27 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:proyecto_final/entities/user.dart';
+
 const String USUARIO_CONSUMIDOR = "usuarioConsumidor";
 
-class DatabaseService {}
+class DatabaseService {
+  final _firestore = FirebaseFirestore.instance;
+
+  late final CollectionReference _usuariosConsumidorRef;
+
+  DatabaseService() {
+    _usuariosConsumidorRef =
+        _firestore.collection(USUARIO_CONSUMIDOR).withConverter<User>(
+            fromFirestore: (snapshots, _) => User.fromJson(
+                  snapshots.data()!,
+                ),
+            toFirestore: (user, _) => user.toJson());
+  }
+
+  Stream<QuerySnapshot> getUsuarios() {
+    return _usuariosConsumidorRef.snapshots();
+  }
+
+  void addUsuario(User user) async {
+    _usuariosConsumidorRef.add(user);
+  }
+}
