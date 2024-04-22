4bcf1257a950fd2622b37625b7f9dc2fc2aa971c
claauzuma <c.zumarraga94@hotmail.com>
2024-04-22 20:20:43 -0300

    Agregando vista de usuario_home, y un metodo que valida si el usuario existe o no, si existe, se lo lleva a esa vista

--- proyecto_final/lib/services/database_sevice.dart
+++ proyecto_final/lib/services/database_sevice.dart
@@ -31,8 +31,39 @@ late final CollectionReference _usersRef;
     return _usuariosConsumidorRef.snapshots();
   }
 
-  void addUsuario(User user) async {
-    _usuariosConsumidorRef.add(user);
+Future<bool> addUsuario(User user) async {
+  try {
+    // Realizar una consulta para verificar si el correo electrónico ya existe
+    var query = await _usuariosConsumidorRef
+        .where('email', isEqualTo: user.email)
+        .get();
+
+    // Verificar si la consulta devolvió algún documento
+    if (query.docs.isEmpty) {
+      // Si no se encontró ningún documento con el mismo correo electrónico, agregar el usuario
+      await _usuariosConsumidorRef.add(user);
+      return true; // Indicar que el usuario se agregó con éxito
+    } else {
+      // Si se encontró algún documento, el correo electrónico ya existe en la base de datos
+      // Aquí puedes manejar la lógica para mostrar un mensaje de error o realizar otras acciones
+      print('El correo electrónico ya existe en la base de datos');
+      return false; // Indicar que hubo un problema al agregar el usuario
+    }
+  } catch (e) {
+    // Manejar cualquier error que ocurra durante el proceso
+    print('Error al agregar el usuario: $e');
+    return false; // Indicar que hubo un problema al agregar el usuario
   }
+}
+
+Future<bool> validarUsuario(String email) async {
+  try {
+    var query = await _usuariosConsumidorRef.where('email', isEqualTo: email).get();
+    return query.docs.isNotEmpty;
+  } catch (e) {
+    print('Error al validar el usuario: $e');
+    return false; // Manejo del error: en este caso, se asume que el usuario no existe
+  }
+}
 
 }
