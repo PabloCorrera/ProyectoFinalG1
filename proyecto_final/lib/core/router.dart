3b34dff40101f45266c43db9eb2ed8e4e1f02705
PabloCorrera <pablo.correra88@gmail.com>
2024-05-01 18:26:19 -0300

    pull main

--- proyecto_final/lib/core/router.dart
+++ proyecto_final/lib/core/router.dart
@@ -3,6 +3,7 @@ import 'package:proyecto_final/pages/login_register_page.dart';
 import 'package:proyecto_final/pages/home_page.dart';
 import 'package:go_router/go_router.dart';
 import 'package:proyecto_final/pages/user_register.dart';
+import 'package:proyecto_final/pages/usuario_cochera_home.dart';
 import 'package:proyecto_final/pages/usuario_home.dart';
 
 final appRouter = GoRouter(routes: [
@@ -34,5 +35,12 @@ final appRouter = GoRouter(routes: [
     path: '/usuarioHome',
     builder: (context, state) => const UsuarioHome(),
   ), 
+
+
+//  GoRoute(
+//     name: UsuarioCocheraHome.name, // Nombre de la ruta corregido
+//     path: '/usuarioCocheraHome', // Ruta corregida
+//     builder: (context, state) => const UsuarioCocheraHome(), // Página corregida
+//   ),
   
 ]);
